adff494baa5a0c9556cfe55c054f603898b63671
alero <amoldovan@itba.edu.ar>
2020-08-11 03:32:05 -0300

    probando entrada de graficos2

--- src/plotTool.py
+++ src/plotTool.py
@@ -29,6 +29,12 @@ class Entrada(Enum):
     BODE = 2
 
 
+class Grafico(Enum):
+    TEORICO = "."
+    LTSPICE = "o"
+    MEDIDO = "s"
+
+
 class PlotTool(QWidget, Ui_Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -50,35 +56,36 @@ class PlotTool(QWidget, Ui_Form):
         self.__cb_habilitarSegundoGrafico()
 
         self.spice_PushButton.clicked.connect(self.__cb_spice)
+        self.medicion_PushButton.clicked.connect(self.__cb_medido)
 
         self.borrarGraficos_PushButton.clicked.connect(self.__borrarGraficos)
         self.__borrarGraficos()
 
-    def __add_plots(self, obj: DataFromFile):
+    def __add_plots(self, obj: DataFromFile,marker):
         size=obj.number_of_plots()
 
         if self.selectorGraficoEntrada_ComboBox.currentIndex() == Entrada.SUP.value:
             for index in range(size):
                 x, y = obj.get_plot(index)
-                self.__add_plot_superior(x, y)
+                self.__add_plot_superior(x, y,marker)
         elif self.selectorGraficoEntrada_ComboBox.currentIndex() == Entrada.INF.value:
             for index in range(size):
                 x, y = obj.get_plot(index)
-                self.__add_plot_inferior(x, y)
+                self.__add_plot_inferior(x, y,marker)
         elif self.selectorGraficoEntrada_ComboBox.currentIndex() == Entrada.BODE.value:
             for index in range(size):
                 x, y = obj.get_plot(index)
                 if index % 2 == 0:
-                    self.__add_plot_superior(x, y)
+                    self.__add_plot_superior(x, y,marker)
                 else:
-                    self.__add_plot_inferior(x, y)
+                    self.__add_plot_inferior(x, y,marker)
 
-    def __add_plot_superior(self,x,y):
-        self.graficoSuperior_Axis.plot(x,y)
+    def __add_plot_superior(self,x,y,marker):
+        self.graficoSuperior_Axis.plot(x,y, marker=marker)
         self.graficoSuperior_Canvas.draw()
 
-    def __add_plot_inferior(self,x,y):
-        self.graficoInferior_Axis.plot(x, y)
+    def __add_plot_inferior(self,x,y,marker):
+        self.graficoInferior_Axis.plot(x,y, marker=marker)
         self.graficoInferior_Canvas.draw()
 
     def __cb_spice(self):
@@ -87,7 +94,17 @@ class PlotTool(QWidget, Ui_Form):
         data = DataFromFile()
         data.load_file(path)
         if data.is_valid():
-            self.__add_plots(data)
+            self.__add_plots(data,Grafico.LTSPICE.value)
+        else:
+            print("Archivo inválido")
+
+    def __cb_medido(self):
+        path, _ = QFileDialog.getOpenFileName(filter="*.csv")
+
+        data = DataFromFile()
+        data.load_file(path)
+        if data.is_valid():
+            self.__add_plots(data,Grafico.MEDIDO.value)
         else:
             print("Archivo inválido")
 
@@ -108,6 +125,7 @@ class PlotTool(QWidget, Ui_Form):
         self.yLabel2_LineEdit.hide()
         self.selectorGraficoEntrada_ComboBox.hide()
         self.graficoInferior_StackedWidget.hide()
+        self.selectorGraficoEntrada_ComboBox.setCurrentIndex(Entrada.SUP.value)
 
     def __borrarGraficos(self):
         self.graficoSuperior_Axis.clear()
