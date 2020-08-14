a59b5b0dcead129a8f0855e084f9447a08db65af
MTripo <mtripoli@itba.edu.ar>
2020-08-14 16:29:23 -0300

    nada importante

--- src/backend/LTSpiceData.py
+++ src/backend/LTSpiceData.py
@@ -29,28 +29,3 @@ class LTSpiceData(DataFromFile):
         var_amplitude = 20 * np.log10(np.abs(var))
         var_fase = np.angle(var, deg=True)
         return var_amplitude, var_fase
-
-
-
-
-l = ltspice.Ltspice(os.path.dirname(__file__)+'\IC_Charge-Discharge.raw')
-# Make sure that the .raw file is located in the correct path
-l.parse()
-
-fig, axes = plt.subplots(1, 1, figsize=(20,7))
-
-for i in range(l._case_num): # Iteration in simulation cases
-    time = l.getTime(i)
-    # Case number starts from zero
-    # Each case has different time point numbers
-    I_cap = l.getData('I(C)',i)
-    #V_cap_max.append(max(V_cap))
-    plt.plot(time, I_cap)
-
-axes.set_xlabel("time [s]")
-axes.set_ylabel("I(C) [A]")
-plt.xlim((0, 150e-3))
-plt.ylim((-0.8, 2.2))
-plt.grid()
-plt.savefig("IC_Charge_R_Equal_0.png")
-plt.show()
