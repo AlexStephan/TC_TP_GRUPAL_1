83d2e5db38e2e2256d8cb8fe1fcdcf80b850698c
alero <amoldovan@itba.edu.ar>
2020-08-15 17:27:12 -0300

    le agrego algunas cosas a transferFunction.py, solo para poder basarnos en eso, y q yo pueda comprobar q el resto del programa funcione bien

--- src/backend/transferFunction.py
+++ src/backend/transferFunction.py
@@ -1,15 +1,21 @@
+import numpy as np
+import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
+
+
 class TransferFunction:
     def __init__(self):
-        pass
+        self.w_arr = np.logspace(0,3,3000)
+        self.t_arr = np.linspace(0,1000,1000) #en mseg
 
     def load_Hs(self,numerator,denominator):
         pass
 
     def set_linear_domain(self, min,max,num):
-        pass
+        self.t_arr = np.linspace(min,max,num)
 
     def set_log_domain(self, min,max,num):
-        pass
+        self.w = np.logspace(min,max,num)
 
     def is_valid(self):
         return True
@@ -19,3 +25,7 @@ class TransferFunction:
 
     def get_aproximated_bode(self): # porque confiamos en Tobi <3
         pass
+
+    def get_Output(self,Input_Expression):
+        pass
+
